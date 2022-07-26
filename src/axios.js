e89006f382a440f3934e329296039be71ba3e01e
agholor-christotle <agholorchristotle@gmail.com>
2022-07-26 01:31:33 +0100

    cloud functions added

--- src/axios.js
+++ src/axios.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'https://us-central1-christotle--clone.cloudfunctions.net/api'
-    // "http://localhost:5001/christotle--clone/us-central1/api",
-});
+  baseURL: "http://localhost:5001/christotle--clone/us-central1/api",
+    });
 
 export default instance;
+
+//'https://us-central1-christotle--clone.cloudfunctions.net/api'
 
