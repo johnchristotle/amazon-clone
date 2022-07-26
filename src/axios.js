6e9388a3c9cfd1e970a7d4964dc8525851ee2d44
agholor-christotle <agholorchristotle@gmail.com>
2022-07-26 16:22:05 +0100

    App successfully deployed to cloud functions

--- src/axios.js
+++ src/axios.js
@@ -7,5 +7,7 @@ const instance = axios.create({
 
 export default instance;
 
-//'https://us-central1-christotle--clone.cloudfunctions.net/api'
-
+// Localhost URL: 'http://localhost:5001/christotle--clone/us-central1/api'
+// Hosting URL: 'https://christotle--clone.web.app'
+// Cloud URL: 'https://us-central1-christotle--clone.cloudfunctions.net/api'
+//Project Console 'https://console.firebase.google.com/project/christotle--clone/overview'
